d02dc7e0cee53ae903e1e6b2fad09decc22ba7d8
Galaxeo <hwjustincheok@gmail.com>
2023-06-08 22:45:55 -0400

    feat: make middle button

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const quarts = Array.from(document.body.querySelectorAll('.quart'));
 const center = document.body.querySelector('.center');
 quarts.forEach(quart => quart.addEventListener('mouseover', function() {
+  center.style.height = '65%';
+  center.style.width = '65%'
+  center.style.top = '17.5%';
+  center.style.left = '17.5%';
+  // center.style.backgroundImage = quart.style.backgroundImage;
+}));
+quarts.forEach(quart => quart.addEventListener('mouseout', function() {
   center.style.height = '70%';
   center.style.width = '70%'
   center.style.top = '15%';
   center.style.left = '15%';
-  console.log(quart.style);
-  // center.style.backgroundImage = quart.style.backgroundImage;
-}));
-quarts.forEach(quart => quart.addEventListener('mouseout', function() {
-  center.style.height = '75%';
-  center.style.width = '75%'
-  center.style.top = '12.5%';
-  center.style.left = '12.5%';
   center.style.backgroundImage = "url('resources/rins_black_hair.png')"
 }));
 console.log(center);
+center.addEventListener("click", function() {
+
+})
 // center.addEventListener('mouseover', function() {
 //   center.innerHTML = "HI!"
 //   center.style.background = "yellow";
